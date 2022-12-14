e94afb74eaa9efa55a507b37859ce693473edb33
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-14 15:34:35 +0600

    Done OTP varification

--- server/config/sendMail.ts
+++ server/config/sendMail.ts
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 import { OAuth2Client } from "google-auth-library";
 //
-import { linkTemplate } from "../utils/emailTemplate";
+import { linkTemplate, otpTemplate } from "../utils/emailTemplate";
 
 
 const OAUTH_PLAYGROUND = `${process.env.MAIL_REDIRECT_URL}`;
@@ -40,16 +40,12 @@ export default async (to: string, url: string, txt: string, type?: string, code?
       },
     });
 
-    const byNumber = `
-      <span>${code}</span>
-    `
-
     // set mail option to send user
     const mailOptions = {
       from: SENDER_MAIL,
       to: to,
       subject: "Medium mail varification",
-      html: type == 'send_code' ? byNumber : linkTemplate(url),
+      html: type == 'send_code' ? otpTemplate(code) : linkTemplate(url),
     };
 
     // finaly get and return the result
@@ -59,71 +55,3 @@ export default async (to: string, url: string, txt: string, type?: string, code?
     console.log(err);
   }
 };
-
-
-
-
-// const nodemailer = require("nodemailer");
-// const { google } = require('googleapis')
-// import { OAuth2Client } from "google-auth-library";
-
-// const OAUTH_PLAYGROUND = `${process.env.MAIL_REDIRECT_URL}`;
-
-// const CLIENT_ID = `${process.env.MAIL_CLIENT_ID}`;
-// const CLIENT_SECRET = `${process.env.MAIL_CLIENT_SECRET}`;
-// const REFRESH_TOKEN = `${process.env.MAIL_REFRESH_TOKEN}`;
-// const SENDER_MAIL = `${process.env.MAIL_CLIENT}`;
-
-// // send mail
-// export default async (to: string, url: string, txt: string, type?: string, code?: string) => {
-//   // call google oAuth class  
-//   const OAuth2 = google.auth.OAuth2
-//   const OAuth2_client = new OAuth2(CLIENT_ID, CLIENT_SECRET);
-
-//   // set refresh token that import from "oAuthPlayground"
-//   OAuth2_client.setCredentials({ refresh_token: REFRESH_TOKEN })
-
-//   try {
-//     // get access token from google oAuth2Client
-//     const accessToken = OAuth2_client.getAccessToken();
-
-//     // create nodemailer transport
-//     const transport = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         type: "OAuth2",
-//         user: SENDER_MAIL,
-//         clientId: CLIENT_ID,
-//         clientSecret: CLIENT_SECRET,
-//         refreshToken: REFRESH_TOKEN,
-//         accessToken: accessToken,
-//       },
-//       tls: {
-//         rejectUnauthorized: false,
-//       },
-//     });
-
-//     // mail template
-//     const byLink = `
-//       <span>${url}</span>
-//     `
-
-//     const byNumber = `
-//       <span>${code}</span>
-//     `
-
-//     // set mail option to send user
-//     const mailOptions = {
-//       from: SENDER_MAIL,
-//       to: to,
-//       subject: "Medium mail varification",
-//       html: type == 'send_code' ? byNumber : byLink,
-//     };
-
-//     // finaly get and return the result
-//     const result = await transport.sendMail(mailOptions);
-//     return result;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
--- server/utils/emailTemplate.ts
+++ server/utils/emailTemplate.ts
@@ -41,3 +41,48 @@ export function linkTemplate(url: string){
      `
    ) 
 }
+
+
+// for OTP validation
+export function otpTemplate(code ?: string){
+   return (
+     `  
+        <div style="display:flex;max-width: 620px; align-items:center;justify-content:center;text-align:center;padding:1rem;padding-bottom:1.5rem;background:#05050514;border-radius:20px;" >
+            <div>
+                <h1 style="color: black;" > Well-come to Medium 2.0 by <a href="https://raihanhosen.com" target="_blank" >Raihan Hosen</a> </h1> 
+                <p style="width: 80%; margin: auto; line-height: 1.4; margin-top: -10px; margin-bottom: 2.5rem;" > Congratulations! You're almost set to start using Medium 2.0. Just use below OTP to validate your email address. </p> 
+
+                
+                <div style="background: #3e3e3e; width: fit-content; margin: auto; color: #e3e3e3; border-radius: 7px; padding: 6px 13px;">
+                    <h1 style="margin: auto; letter-spacing: 10px;" > ${code} </h1> 
+                </div>
+
+
+                <div style="margin-top: 3.5rem;" >
+                
+                    <a href="https://github.com/raihanhosen011" target="_blank" >
+                        <img width="25px" height="25px" style="margin-right: 15px;" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" />   
+                    </a> 
+                
+                    <a href="https://www.linkedin.com/in/raihanhosen01/" target="_blank" >
+                        <img width="25px" height="25px" style="margin-right: 15px;" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />   
+                    </a> 
+                
+                    <a href="https://twitter.com/raihanhosen01" target="_blank" >
+                        <img width="25px" height="25px" style="margin-right: 15px;" src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Twitter_Logo.png" />   
+                    </a> 
+                
+                    <a href="https://raihanhosen.com/" target="_blank" >
+                        <img width="25px" height="25px" style="margin-right: 15px;" src="https://www.freepnglogos.com/uploads/logo-internet-png/logo-internet-clipart-internet-logo-clipground-33.jpg" />   
+                    </a> 
+                
+                    <a href="https://www.facebook.com/raihanhosen01" target="_blank" >
+                        <img width="25px" height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png" />   
+                    </a> 
+                    
+                </div>
+            </div>
+        </div>
+     `
+   ) 
+}
