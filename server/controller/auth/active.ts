f914c686235d5f834ce589f924735060118d4feb
raihanhosen011 <raihanhosen011@gmail.com>
2022-12-17 17:56:45 +0600

    change active token function

--- server/controller/auth/active.ts
+++ server/controller/auth/active.ts
@@ -9,20 +9,18 @@ import user from "../../models/user";
 
 export default async (req: Request, res: Response) => {
     try {
-      const { active_token } = req.body
+      const { id } = req.params
       
       // verify active token -
-      const { new_user } = <IDecoded>await jwt.verify(active_token, `${process.env.ACTIVE_TOKEN_SECRET}`)
+      const { new_user } = <IDecoded>await jwt.verify(id, `${process.env.ACTIVE_TOKEN_SECRET}`)
       
-      // save data from active token
-      const registred = new user(new_user).save()
-
-      if(!registred){
-        res.status(200).json({ success : false, msg : "Authentication failed. Please do registation again" })  
+      if(new_user){
+          // save data from active token
+          new user(new_user).save()
+          res.status(200).json({ success : true, msg : "Account successfully activate!" })  
       }else{
-        res.status(200).json({ success : true, msg : "Account successfully activate!" })  
+          res.status(200).json({ success : false, msg : "Authentication failed. Please do registation again" })  
       }
-
     } catch (e) {
        return res.status(500).json({ msg : (e as Error).message, success : false }) 
     }
